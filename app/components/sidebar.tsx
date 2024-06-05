6831b41d61d2b16abd51b9586f4391d5da2c1df1
Xiaobing Mi <xiaomi@xingke888.com>
2024-06-05 15:47:35 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
         香港高才联合会AI办公平台
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          基于最新的gpt-4o
+          基于GPT-4o
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
