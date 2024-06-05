da61508a657f8f71d0e88cbc1254e04b3ef05a71
Xiaobing Mi <xiaomi@xingke888.com>
2024-06-05 16:04:49 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -8,12 +8,12 @@ const cn = {
   Error: {
     Unauthorized: isApp
       ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确。"
-      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页填入你自己的 OpenAI API Key。",
+      : "访问密码不正确或为空，请点击此处[登录页](/#/auth)输入访问密码，请联系联合会获取访问密码。",
   },
   Auth: {
     Title: "需要密码",
     Tips: "管理员开启了密码验证，请在下方填入访问码",
-    SubTips: "或者输入你的 OpenAI 或 Google API 密钥",
+    SubTips: "或者输入你的 OpenAI 或 Google API 密钥（输入访问码就不需要输入密钥）",
     Input: "在此处填写访问码",
     Confirm: "确认",
     Later: "稍后再说",
